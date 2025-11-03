efd6a57becc33010a609cfc3ec985f44eabbc065
Martin Maynard <martintm@Martins-MacBook-Air.local>
2025-11-02 22:35:50 -0500

    fixed platform.js

--- Platform.js
+++ Platform.js
@@ -1,4 +1,15 @@
 class Platform {
-  constructor(x,y,w,h){ this.x=x; this.y=y; this.w=w; this.h=h; }
-  display(){ noStroke(); fill(80); rect(this.x, this.y, this.w, this.h); }
+  constructor(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+  }
+
+  display() {
+    noStroke();
+    fill(135, 140, 141);
+    rectMode(CORNER);
+    rect(this.x, this.y, this.w, this.h);
+  }
 }
