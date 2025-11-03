a8f65546159b2385017c4642f106df01e37a21e5
Martin Maynard <martintm@Martins-MacBook-Air.local>
2025-11-02 22:14:04 -0500

    sketch.js rework

--- sketch.js
+++ sketch.js
@@ -2,7 +2,6 @@ let gameState = 0; // 0 = Menu, 1 = Game, 2 = Credits
 let startButton, creditsButton;
 
 let bgMusic, damageSound;
-
 let player;
 let ground, platform1, platform2, platform3, platform4, platform5, platform6, platform7, platform8, platform9;
 let ladder, ladder2, ladder3, ladder4, ladder5, ladder6, ladder7, ladder8;
@@ -10,16 +9,17 @@ let enemy, enemy2, enemy3, enemy4, enemy5;
 let power;
 let goal;
 let boss;
+let gameWon = false;
 
 let playerImg, enemyImg, bossImg;
 
 function preload() {
-  soundFormats('mp3','wav','ogg');
+  soundFormats('mp3', 'wav', 'ogg');
   bgMusic = loadSound('assets/background.wav');
   damageSound = loadSound('assets/damage.wav');
   playerImg = loadImage('assets/player.png');
-  enemyImg  = loadImage('assets/enemy.png'); 
-  bossImg   = loadImage('assets/boss.png');
+  enemyImg = loadImage('assets/enemy.png');
+  bossImg = loadImage('assets/boss.png');
 }
 
 function setup() {
@@ -27,63 +27,173 @@ function setup() {
   textAlign(CENTER, CENTER);
   rectMode(CENTER);
 
-  startButton = new Button(width/2, height/2 - 40, 200, 60, 'Start');
-  creditsButton = new Button(width/2, height/2 + 40, 200, 60, 'Credits');
+  // Buttons
+  startButton = new Button(width / 2, height / 2 - 40, 200, 60, "Start");
+  creditsButton = new Button(width / 2, height / 2 + 40, 200, 60, "Credits");
 
-  ground = new Platform(width/2, height - 20, width, 40);
-  platform1 = new Platform(150, 480, 220, 20);
-  platform2 = new Platform(400, 420, 180, 20);
-  platform3 = new Platform(650, 360, 220, 20);
-  platform4 = new Platform(120, 300, 160, 20);
-  platform5 = new Platform(330, 260, 200, 20);
-  platform6 = new Platform(580, 220, 160, 20);
-  platform7 = new Platform(740, 160, 120, 20);
-  platform8 = new Platform(420, 140, 160, 20);
-  platform9 = new Platform(220, 100, 140, 20);
+  // Player
+  player = new Player(10, 500, playerImg, damageSound);
 
-  ladder  = new Ladder(150, 440, 40, 80);
-  ladder2 = new Ladder(650, 320, 40, 80);
-  ladder3 = new Ladder(120, 260, 40, 80);
-  ladder4 = new Ladder(330, 220, 40, 80);
-  ladder5 = new Ladder(580, 180, 40, 80);
-  ladder6 = new Ladder(740, 120, 40, 80);
-  ladder7 = new Ladder(420, 100, 40, 80);
-  ladder8 = new Ladder(220,  60, 40, 80);
+  // Platforms
+  ground = new Platform(0, 590, 800, 10);
+  platform1 = new Platform(400, 515, 500, 10);
+  platform2 = new Platform(450, 450, 500, 10);
+  platform3 = new Platform(390, 325, 150, 10);
+  platform4 = new Platform(325, 400, 125, 10);
+  platform5 = new Platform(200, 500, 150, 10);
+  platform6 = new Platform(0, 400, 200, 10);
+  platform7 = new Platform(50, 325, 250, 10);
+  platform8 = new Platform(530, 250, 275, 10);
+  platform9 = new Platform(0, 175, 800, 10);
 
-  enemy  = new Enemy(500, 380, enemyImg);
-  enemy2 = new Enemy(250, 460, enemyImg);
-  enemy3 = new Enemy(650, 320, enemyImg);
-  enemy4 = new Enemy(330, 220, enemyImg);
-  enemy5 = new Enemy(120, 260, enemyImg);
+  // Ladders
+  ladder = new Ladder(400, 475, 20, 115);
+  ladder2 = new Ladder(585, 450, 20, 65);
+  ladder3 = new Ladder(200, 400, 20, 100);
+  ladder4 = new Ladder(450, 400, 20, 50);
+  ladder5 = new Ladder(50, 325, 20, 75);
+  ladder6 = new Ladder(375, 325, 20, 75);
+  ladder7 = new Ladder(520, 250, 20, 75);
+  ladder8 = new Ladder(750, 175, 20, 75);
 
-  power = new PowerUp(60, 560, 20);
-  goal  = new Goal(760, 80, 30);
+  // Power-Up
+  power = new PowerUp(0, 0);
+  if (power.randomSpawn) power.randomSpawn();
 
-  boss = new Boss(700, 80, bossImg);
+  // Enemies
+  enemy = new Enemy(500, 560, 200, 700, enemyImg);
+  enemy2 = new Enemy(500, 485, 500, 775, enemyImg);
+  enemy3 = new Enemy(700, 420, 575, 750, enemyImg);
+  enemy4 = new Enemy(10, 370, 10, 150, enemyImg);
+  enemy5 = new Enemy(550, 220, 550, 775, enemyImg);
 
-  player = new Player(60, height - 60, playerImg);
+  // Boss
+  boss = new Boss(width / 2 - 50, 30, bossImg);
 
-  if (bgMusic && !bgMusic.isPlaying()) bgMusic.loop();
+  // Goal
+  goal = new Goal(0, 135);
+
+  if (!bgMusic.isPlaying()) bgMusic.loop();
 }
 
 function draw() {
-  background(30);
-
   if (gameState === 0) {
-    fill(255);
-    textSize(48);
-    text('CCHW2', width/2, height/2 - 140);
-    startButton.update(mouseX, mouseY);
-    creditsButton.update(mouseX, mouseY);
-    startButton.display();
-    creditsButton.display();
+    drawMenu();
   } else if (gameState === 1) {
-    updateGame();
-    renderGame();
+    drawGame();
   } else if (gameState === 2) {
+    drawCredits();
+  }
+}
+
+function drawMenu() {
+  rectMode(CENTER);
+  background(30);
+  fill(255);
+  textSize(48);
+  text("Super Grandpa 64", width / 2, 150);
+
+  startButton.update(mouseX, mouseY);
+  creditsButton.update(mouseX, mouseY);
+  startButton.display();
+  creditsButton.display();
+}
+
+function drawCredits() {
+  rectMode(CENTER);
+  background(30);
+  fill(255);
+  textSize(32);
+  text("Credits", width / 2, 100);
+  textSize(20);
+  text("Game created by Martin Maynard", width / 2, 200);
+  text("Art by Martin Maynard", width / 2, 240);
+  text("Music & Sounds from Freesound.org", width / 2, 260);
+  text("Press any key to return", width / 2, 400);
+}
+
+function drawGame() {
+  rectMode(CORNER);
+  background(135, 206, 235);
+
+  if (gameWon) {
+    background(30);
+    fill(255, 255, 0);
+    textSize(48);
+    text("YOU WIN!", width / 2, height / 2);
+    textSize(20);
     fill(255);
-    textSize(24);
-    text('Credits\nPress any key to return', width/2, height/2);
+    text("Press any key to return", width / 2, height / 2 + 60);
+    return;
+  }
+
+  // Platforms
+  ground.display();
+  platform1.display();
+  platform2.display();
+  platform3.display();
+  platform4.display();
+  platform5.display();
+  platform6.display();
+  platform7.display();
+  platform8.display();
+  platform9.display();
+
+  // Ladders
+  ladder.display();
+  ladder2.display();
+  ladder3.display();
+  ladder4.display();
+  ladder5.display();
+  ladder6.display();
+  ladder7.display();
+  ladder8.display();
+
+  // Enemies
+  enemy.update(); enemy.display();
+  enemy2.update(); enemy2.display();
+  enemy3.update(); enemy3.display();
+  enemy4.update(); enemy4.display();
+  enemy5.update(); enemy5.display();
+
+  // Boss
+  boss.update();
+  boss.display();
+
+  // Player
+  const platforms = [ground, platform1, platform2, platform3, platform4, platform5, platform6, platform7, platform8, platform9];
+  const ladders = [ladder, ladder2, ladder3, ladder4, ladder5, ladder6, ladder7, ladder8];
+  player.update(platforms, ladders);
+  player.display();
+
+  // Collisions
+  if (
+    enemy.collidesWith(player) ||
+    enemy2.collidesWith(player) ||
+    enemy3.collidesWith(player) ||
+    enemy4.collidesWith(player) ||
+    enemy5.collidesWith(player)
+  ) {
+    player.takeDamage();
+  }
+
+  for (const bp of boss.projectiles) {
+    if (dist(bp.x, bp.y, player.pos.x + player.w / 2, player.pos.y + player.h / 2) < 15) {
+      player.takeDamage();
+    }
+  }
+
+  // Power-up
+  if (power.active && power.collidesWith(player)) {
+    power.active = false;
+    player.activateInvincibility(400); // 8 seconds
+  }
+  power.display();
+
+  // Goal
+  goal.display();
+  if (goal.collidesWith(player)) {
+    gameWon = true;
   }
 }
 
@@ -91,7 +201,7 @@ function mousePressed() {
   if (gameState === 0) {
     if (startButton.isClicked(mouseX, mouseY)) {
       gameState = 1;
-      if (bgMusic && !bgMusic.isPlaying()) bgMusic.loop();
+      if (!bgMusic.isPlaying()) bgMusic.loop();
     } else if (creditsButton.isClicked(mouseX, mouseY)) {
       gameState = 2;
     }
@@ -99,57 +209,8 @@ function mousePressed() {
 }
 
 function keyPressed() {
-  if (gameState === 2) gameState = 0;
-}
-
-function updateGame() {
-  const platforms = [ground,platform1,platform2,platform3,platform4,platform5,platform6,platform7,platform8,platform9];
-  const ladders   = [ladder,ladder2,ladder3,ladder4,ladder5,ladder6,ladder7,ladder8];
-  const enemies   = [enemy,enemy2,enemy3,enemy4,enemy5];
-
-  player.update(platforms, ladders);
-  enemies.forEach(e => e.update());
-  boss.update(player);
-
-  if (power && player.overlapsCircle(power.x, power.y, power.size)) {
-    power = null;
+  if (gameState === 2 || (gameWon && keyIsPressed)) {
+    gameWon = false;
+    gameState = 0;
   }
-
-  if (goal && dist(player.x, player.y, goal.x, goal.y) < goal.r + player.w*0.5) {
-    gameState = 2;
-  }
-
-  enemies.forEach(e => {
-    if (player.overlapsBox(e.x, e.y, e.w, e.h)) {
-      if (damageSound) damageSound.play();
-      player.hit();
-    }
-  });
-
-  boss.projectiles.forEach(p => {
-    if (player.overlapsCircle(p.x, p.y, p.r)) {
-      if (damageSound) damageSound.play();
-      player.hit();
-      p.dead = true;
-    }
-  });
-  boss.projectiles = boss.projectiles.filter(p => !p.dead);
-}
-
-function renderGame() {
-  ground.display();
-  platform1.display(); platform2.display(); platform3.display();
-  platform4.display(); platform5.display(); platform6.display();
-  platform7.display(); platform8.display(); platform9.display();
-
-  ladder.display(); ladder2.display(); ladder3.display(); ladder4.display();
-  ladder5.display(); ladder6.display(); ladder7.display(); ladder8.display();
-
-  if (goal) goal.display();
-  if (power) power.display();
-
-  enemy.display(); enemy2.display(); enemy3.display(); enemy4.display(); enemy5.display();
-  boss.display();
-
-  player.display();
 }
